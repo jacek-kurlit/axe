8136ebea71b4c01409072845aa1ba4c7af8b5e13
maksiuk <jacek.kurlit@gmail.com>
2024-09-28 22:14:44 +0200

    updated help docs

--- src/cli.rs
+++ src/cli.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use clap::{arg, command};
 
 use clap::Parser;
@@ -7,11 +9,27 @@ use clap::Parser;
 pub struct Cli {
     #[arg(default_value = "echo")]
     pub cmd: String,
+    /// Arguments templates that will be resolved and passed to cmd.
+    /// Allowed values:
+    /// - static text
+    /// - {} - all input arguments
+    /// - {<sep>} - all input arguments, each splittded by <sep> and all parts of split are taken
+    /// - {<sep>y} - all input arguments, each splittded by <sep> and y-th part of split is taken
+    /// - {x} - x-th argument
+    /// - {x<sep>} - x-th argument splittded by <sep> and all parts of split are taken
+    /// - {x<sep>y} - x-th argument splittded by <sep> and y-th part of split is taken.
+    #[arg(verbatim_doc_comment)]
     pub args_templates: Vec<String>,
+    /// Separator between args. Each entry line will be splitted by this separator
     #[arg(short, long, default_value = " ")]
     pub args_separator: String,
+    /// Separator between entries. Each entry will be splitted by args_separator to produce cmd args
     #[arg(short, long, default_value = "\n")]
     pub entries_separator: String,
+    /// Print command with resolved args instead of running it
     #[arg(short, long)]
     pub debug: bool,
+    /// Reads arguments from file instead of standard input
+    #[arg(short = 'f', long = "args-file", value_name = "FILE")]
+    pub file_args: Option<PathBuf>,
 }
